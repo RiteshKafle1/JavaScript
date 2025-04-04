4ff06959e18aa14f6d35e35fc089ee6407c17be8
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-04-05 00:24:16 +0545

    hoisting

---
+++ 05/Hoisting.js
@@ -0,0 +1,17 @@
+// sayMyName()
+// function sayMyName(){
+//   console.log("I am hoisting");
+// }
+// func declaration shift to the top in the scope.
+
+
+// console.log(age); // undefined. -> variable declaration shift to the top
+// var age =25;
+
+// console.log(age);// Reference error
+// let age=60;
+
+// sayHello(); 
+// let sayHello=function(){
+//   console.log('Hello ,How are you'); // reference error.
+// }
